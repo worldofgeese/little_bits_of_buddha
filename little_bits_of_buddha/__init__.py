b248c556e669451f210cf45b008d178669a7ba8a
worldofgeese <5-worldofgeese@users.noreply.gitlab.hansen.agency>
2022-02-02 20:24:50 +0100

    The program itself
    Here I've tried to define my own function that loads my JSON object as a
    Python dictionary and then prints a random sutta from the Majjhima
    Nikāya. I've incorporated Jack's suggested fix but receive the following
    error:
    ```
    Traceback (most recent call last):
      File "/home/taohansen/projects/little-bits-of-buddha/little_bits_of_buddha/__init__.py", line 13, in <module>
        print(random_sutta())
      File "/home/taohansen/projects/little-bits-of-buddha/little_bits_of_buddha/__init__.py", line 10, in random_sutta
        random.choice(data["collection"]["MN"].values())
      File "/nix/store/i6vabb4div9iy6lsl642d86k1q8riasn-python3-3.9.9/lib/python3.9/random.py", line 346, in choice
        return seq[self._randbelow(len(seq))]
    TypeError: 'dict_values' object is not subscriptable
    ```

--- little_bits_of_buddha/__init__.py
+++ little_bits_of_buddha/__init__.py
@@ -1,1 +1,13 @@
-__version__ = '0.1.0'
+__version__ = "0.1.0"
+
+import json
+import random
+
+
+def random_sutta():
+    with open("../data.json") as json_file:
+        data = json.load(json_file)
+        random.choice(data["collection"]["MN"].values())
+
+
+print(random_sutta())
