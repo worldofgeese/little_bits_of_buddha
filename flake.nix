7b41e4c9755a8ba3a4850fa41ea8980a26d7b80b
worldofgeese <5-worldofgeese@users.noreply.gitlab.hansen.agency>
2022-02-08 18:23:52 +0100

    Update flake to include Python devtools
    Flake now includes the full suite of Python packages including a
    language server, pyright and tools to facilitate testing and formatting.

--- flake.nix
+++ flake.nix
@@ -26,6 +26,18 @@
 
         defaultApp = pkgs.myapp;
 
-        devShell = pkgs.mkShell { buildInputs = [ pkgs.python3 pkgs.poetry ]; };
+        devShell = with pkgs;
+          pkgs.mkShell {
+            buildInputs = [
+              python3
+              poetry
+              python39Packages.isort # A Python utility / library to sort Python imports
+              python39Packages.pyflakes # A simple program which checks Python source files for errors
+              python39Packages.nose # A unittest-based testing framework for python that makes writing and running tests easier
+              python39Packages.pytest # Framework for writing tests
+              nodePackages.pyright # Type checker for the Python language
+              python39Packages.black # The uncompromising Python code formatter
+            ];
+          };
       }));
 }
