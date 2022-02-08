3cc0d840c608b7cc6f9c50ed674fd10391ff39db
worldofgeese <5-worldofgeese@users.noreply.gitlab.hansen.agency>
2022-02-08 18:19:21 +0100

    Collections module returns attributed quotes

--- little_bits_of_buddha/__init__.py
+++ little_bits_of_buddha/__init__.py
@@ -7,7 +7,11 @@ import random
 def random_sutta():
     with open("../data.json") as json_file:
         data = json.load(json_file)
-        random.choice(data["collection"]["MN"].values())
+
+        attributed_quotes = []
+        for collection in data["collection"].values():
+            for quote in collection.values():
+                attributed_quotes.append(quote)
+        return random.choice(attributed_quotes)
 
 
-print(random_sutta())
