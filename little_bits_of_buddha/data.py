63eae41f7d0449d6fe0392c70f256d945fdcb8a9
worldofgeese <5-worldofgeese@users.noreply.gitlab.hansen.agency>
2022-02-17 18:33:25 +0100

    Use importlib
    The `open()` function takes a relative file path, which isn't
    deterministic. This uses `importlib` instead to access my data file from
    the project.

--- little_bits_of_buddha/data.py
+++ little_bits_of_buddha/data.py
@@ -2,14 +2,15 @@
 
 import json
 import random
+from importlib import resources
+
+with resources.open_text("little_bits_of_buddha", "data.json") as json_file:
+    data = json.load(json_file)
 
 
 def random_sutta():
-    with open("little_bits_of_buddha/data.json") as json_file:
-        data = json.load(json_file)
-
-        attributed_quotes = []
-        for collection in data["collection"].values():
-            for quote in collection.values():
-                attributed_quotes.append(quote)
-        return random.choice(attributed_quotes)
+    attributed_quotes = []
+    for collection in data["collection"].values():
+        for quote in collection.values():
+            attributed_quotes.append(quote)
+    return random.choice(attributed_quotes)
