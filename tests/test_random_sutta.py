f2a938180fd1a32b9c628ccc783df1ad1a66466f
Tao Hansen <5-worldofgeese@users.noreply.gitlab.hansen.agency>
2023-02-04 23:49:53 +0100

    Resolve "No module named  little_bits_of_buddha_worldofgeese"

--- tests/test_random_sutta.py
+++ tests/test_random_sutta.py
@@ -1,19 +1,25 @@
-from little_bits_of_buddha.data import random_sutta
+import pytest
 
+from little_bits_of_buddha_worldofgeese.data import random_sutta
 
-def test_sutta():
-    assert random_sutta(
-        {
-            "collection": {
-                "MN": {
-                    "27": {
-                        "link": "https://www.dhammatalks.org/suttas/MN/MN27.html",
-                        "quote": "Abandoning divisive speech he abstains from divisive speech. What he has heard here he does not tell there to break those people apart from these people here. What he has heard there he does not tell here to break these people apart from those people there. Thus reconciling those who have broken apart or cementing those who are united, he loves concord, delights in concord, enjoys concord, speaks things that create concord.",
-                    },
+SUTTAS = {
+        "collection": {
+            "MN": {
+                "27": {
+                    "link": "https://www.dhammatalks.org/suttas/MN/MN27.html",
+                    "quote": "Abandoning divisive speech he abstains from divisive speech. What he has heard here he does not tell there to break those people apart from these people here. What he has heard there he does not tell here to break these people apart from those people there. Thus reconciling those who have broken apart or cementing those who are united, he loves concord, delights in concord, enjoys concord, speaks things that create concord.",
                 },
-            }
+            },
         }
-    ) == {
+    }
+
+@pytest.mark.parametrize("expected_sutta", [
+    ({
         "link": "https://www.dhammatalks.org/suttas/MN/MN27.html",
         "quote": "Abandoning divisive speech he abstains from divisive speech. What he has heard here he does not tell there to break those people apart from these people here. What he has heard there he does not tell here to break these people apart from those people there. Thus reconciling those who have broken apart or cementing those who are united, he loves concord, delights in concord, enjoys concord, speaks things that create concord.",
-    }
+    }),
+]
+)
+def test_sutta(expected_sutta):
+    
+    assert random_sutta(suttas=SUTTAS) == expected_sutta
