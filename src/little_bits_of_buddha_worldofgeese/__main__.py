f5c94ce8a16203aa7604bb8477e9d7ac8598e2c8
worldofgeese <5-worldofgeese@users.noreply.gitlab.hansen.agency>
2023-03-09 11:40:34 +0100

    Use trio directly without hypercorn
    We don't expose an HTTP endpoint so we don't need a webserver fronting trio

--- src/little_bits_of_buddha_worldofgeese/__main__.py
+++ src/little_bits_of_buddha_worldofgeese/__main__.py
@@ -1,15 +1,10 @@
 import trio
 import triogram
-from fastapi import FastAPI
-from hypercorn.config import Config as HyperConfig
-from hypercorn.trio import serve
 
 from little_bits_of_buddha_worldofgeese.chatbot import the_buddha
 
-app = FastAPI()
 
 
-@app.get("/")
 async def main():
     """
     Starts the bot and event handlers.
@@ -24,7 +19,4 @@ async def main():
 
 
 if __name__ == "__main__":
-    print("Starting server...")
-    config = HyperConfig()
-    config.bind = [f"0.0.0.0:8000"]
-    trio.run(serve, app, config)
+    trio.run(main)
